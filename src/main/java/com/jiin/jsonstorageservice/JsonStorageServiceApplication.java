62e9ffaf72146a2d38394d9e315d95a13de2d0e5
jiinkim <jiin.kim109@gmail.com>
2022-05-03 10:38:32 -0700

    initial commit

---
+++ src/main/java/com/jiin/jsonstorageservice/JsonStorageServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jiin.jsonstorageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonStorageServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsonStorageServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jiin/jsonstorageservice/JsonStorageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiin.jsonstorageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonStorageServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
